bb6b5ca046fdea4555dce8038f407387b1f23447
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-04-16 11:07:37 -0600

    Added conditional rendering to habitat and fun fact

--- src/Components/Bird/Bird.js
+++ src/Components/Bird/Bird.js
@@ -22,7 +22,7 @@ class Bird extends Component {
 			<div className='single-bird-container'>
 				{/* <p>{this.state.birdID}</p> */}
 				<div className='name-img-container'>
-					<div className='name-container'>
+					<div className='single-name-container'>
 						<h3 className='bird-name'>{this.state.singleBird.common_name}</h3>
 						<p className='bird-sci-name'>
 							{this.state.singleBird.scientific_name}
@@ -37,7 +37,25 @@ class Bird extends Component {
 						id='birdImg'
 					/>
 				</div>
-				<p className='description'>{this.state.singleBird.description}</p>
+				<p className='description'>
+					<p className='intro'>Description: </p>
+					<br></br>
+					{this.state.singleBird.description}
+				</p>
+				{this.state.singleBird.habitat && (
+					<p className='description'>
+						<p className='intro'>Habitat: </p>
+						<br></br>
+						{this.state.singleBird.habitat}
+					</p>
+				)}
+				{this.state.singleBird.fun_fact && (
+					<p className='description'>
+						<p className='intro'>Fun Fact: </p>
+						<br></br>
+						{this.state.singleBird.fun_fact}
+					</p>
+				)}
 			</div>
 		)
 	}
