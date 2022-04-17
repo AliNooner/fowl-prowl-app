5ee92e23ed7f086ff0797c0a29a315e58877e0c5
Nicole Valentini <ncvalentini@gmail.com>
2022-04-17 13:17:09 -0600

    Add filter and view for bird families

--- src/Components/Learn/Learn.js
+++ src/Components/Learn/Learn.js
@@ -3,9 +3,13 @@ import './Learn.css'
 import AllBirds from '../AllBirds/AllBirds'
 
 const Learn = ({ allBirds, changeIcon }) => {
+ const titFamily = allBirds.filter(bird => bird.family === 'Paridae')
+ const bopFamily = allBirds.filter(bird => bird.family === 'Bop')
+	const falconFamily = allBirds.filter(bird => bird.family ==='Falconidae')
+
 	return (
 		<section className='learnPage'>
-			<p className='select-bird-p'>Select a bird to learn more</p>
+			<p className='select-bird-p'>Welcome to the Fowl Prowl encyclopedia! Click the 'learn more' button for additional information.</p>
 			<label for='select-family'>Filter by family:</label>
 			<select className='select-family' name='select-family'>
 				<option value='select'>select</option>
@@ -13,8 +17,14 @@ const Learn = ({ allBirds, changeIcon }) => {
 				<option value='Paridae'>Tit Family</option>
 				<option value='Falconidae'>Falcon Family</option>
 			</select>
-
-			<AllBirds allBirds={allBirds} changeIcon ={changeIcon} />
+			<div>
+				<p className = "familyTitle"> The Paridae Family</p>
+				<AllBirds allBirds={titFamily} changeIcon ={changeIcon} />
+				<p className = "familyTitle"> The Paradisaeidae Family</p>
+				<AllBirds allBirds={bopFamily} changeIcon ={changeIcon} />
+				<p className = "familyTitle"> Falconidae Family</p>
+				<AllBirds allBirds={falconFamily} changeIcon ={changeIcon} />
+			</div>
 		</section>
 	)
 }
