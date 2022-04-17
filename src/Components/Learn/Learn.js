be09a70f55c54dd9beb0e6631fcfc19c2a9929cf
Nicole Valentini <ncvalentini@gmail.com>
2022-04-17 13:36:14 -0600

    Add headers to family sections

--- src/Components/Learn/Learn.js
+++ src/Components/Learn/Learn.js
@@ -10,7 +10,7 @@ const Learn = ({ allBirds, changeIcon }) => {
 	return (
 		<section className='learnPage'>
 			<p className='select-bird-p'>Welcome to the Fowl Prowl encyclopedia! Click the 'learn more' button for additional information.</p>
-			<label for='select-family'>Filter by family:</label>
+			<label>Filter by family:</label>
 			<select className='select-family' name='select-family'>
 				<option value='select'>select</option>
 				<option value='Bop'>Birds of Paradise</option>
@@ -18,11 +18,11 @@ const Learn = ({ allBirds, changeIcon }) => {
 				<option value='Falconidae'>Falcon Family</option>
 			</select>
 			<div>
-				<p className = "familyTitle"> The Paridae Family</p>
+				<p className = "family-title"> The Paridae Family:</p>
 				<AllBirds allBirds={titFamily} changeIcon ={changeIcon} />
-				<p className = "familyTitle"> The Paradisaeidae Family</p>
+				<p className = "family-title"> The Paradisaeidae Family:</p>
 				<AllBirds allBirds={bopFamily} changeIcon ={changeIcon} />
-				<p className = "familyTitle"> Falconidae Family</p>
+				<p className = "family-title"> The Falconidae Family:</p>
 				<AllBirds allBirds={falconFamily} changeIcon ={changeIcon} />
 			</div>
 		</section>
