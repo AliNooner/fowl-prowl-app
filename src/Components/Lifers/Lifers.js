e8a4e6dd807c01191cfee2c24167cbd949ec2bbd
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-04-16 18:50:38 -0600

    Add fav button animation

--- src/Components/Lifers/Lifers.js
+++ src/Components/Lifers/Lifers.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import Card from '../Card/Card';
-import './Lifers.css';
+import React from 'react'
+import Card from '../Card/Card'
+import './Lifers.css'
 
-const Lifers = ({allBirds, changeIcon}) => {
-  const birdCards = allBirds.map((bird) => {
-    if (bird.isFavorited){
-      return (
-        <Card
-        birdObject = {bird}
-        commonName={bird.common_name}
-        scientificName={bird.scientific_name}
-        img={bird.img_url}
-        id={bird.id}
-        key={bird.id}
-        changeIcon={changeIcon}
-        />
-      )
-    }
-  })
-
-  return <div className='allBirds-container'>{birdCards}</div>
+const Lifers = ({ allBirds, changeIcon }) => {
+	const birdCards = allBirds.map((bird) => {
+		if (bird.isFavorited) {
+			return (
+				<Card
+					birdObject={bird}
+					commonName={bird.common_name}
+					scientificName={bird.scientific_name}
+					img={bird.img_url}
+					id={bird.id}
+					key={bird.id}
+					changeIcon={changeIcon}
+				/>
+			)
+		}
+	})
+	return <div className='allBirds-container'>{birdCards}</div>
 }
 
-
-
-
-export default Lifers;
+export default Lifers
